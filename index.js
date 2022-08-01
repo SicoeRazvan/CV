1451357da74f377b3a94c7895a3c871e56a24a4c
Sicoe Razvan <sikoe.razvan@gmail.com>
2022-08-01 16:17:16 +0300

    Refactor js

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-
-function showPage(id){
-  console.warn("Please show me this page " +id);
-  hideAllPages();
-  show(id);
-}
-
-function hideAllPages() {
-  hide("home");
-  hide("skills");
-  hide("projects");
-  hide("languages");
-}
-
 function hide(id) {
   document.getElementById(id).style.display = "none";
 }
-
 function show(id) {
   document.getElementById(id).style.display = "block";
 }
 
-showPage("home");
+var activePage = "home";
+
+function showPage(nextPage) {
+  console.warn("change", activePage, "to", nextPage);
+  hide(activePage);
+  show(nextPage);
+  document
+    .querySelector(`a[data-page=${activePage}]`)
+    .classList.remove("active");
+  document.querySelector(`a[data-page=${nextPage}]`).classList.add("active");
+  activePage = nextPage;
+}
+
+function initEvents() {
+  document
+    .getElementById("top-menu-bar")
+    .addEventListener("click", function (e) {
+      if (e.target.matches("a")) {
+        var id = e.target.getAttribute("data-page");
+        showPage(id);
+      }
+    });
+}
+
+showPage(activePage);
+initEvents();
